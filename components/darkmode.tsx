259f449f450808acd8540e421aff7671bbc496e2
Mikael Øverland <overlandmikael@gmail.com>
2026-02-06 22:24:26 +0100

    bakgrunn og start på darkmode

---
+++ components/darkmode.tsx
@@ -0,0 +1,16 @@
+import { button } from "motion/react-client";
+import { useState } from "react";
+
+
+export default function Darkmode(){
+
+    return(
+        <button>
+            light
+        </button>
+    )
+
+
+
+
+}
---
+++ components/mainBcx.tsx
@@ -0,0 +1,8 @@
+export default function MainBox(){
+    return (
+        <div className="h-100 w-1/2 bg-dark_amethyst border-10 border-purple-900 z-10">
+            <div className="h-10 bg-purple-950">
+            </div>
+        </div>
+    )
+}
